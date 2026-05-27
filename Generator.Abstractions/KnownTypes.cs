9ca6c754d867753f0417f22ac6e554f950ada88f
juner <juner@juner.net>
2026-05-27 15:05:23 +0900

    feat: standardize common generator types in Esolang.Generator.Abstractions

--- Generator.Abstractions/KnownTypes.cs
+++ Generator.Abstractions/KnownTypes.cs
@@ -3,14 +3,34 @@ using Microsoft.CodeAnalysis;
 namespace Esolang.Generator;
 
 /// <summary>
-/// Provides a base structure for resolving types from a <see cref="Compilation"/>.
+/// Defines metadata names for commonly used types in esolang source generators.
 /// </summary>
-public readonly struct KnownTypes
+public static class KnownTypeNames
+{
+    public const string Task = "System.Threading.Tasks.Task";
+    public const string TaskT = "System.Threading.Tasks.Task`1";
+    public const string ValueTask = "System.Threading.Tasks.ValueTask";
+    public const string ValueTaskT = "System.Threading.Tasks.ValueTask`1";
+    public const string IEnumerableT = "System.Collections.Generic.IEnumerable`1";
+    public const string IAsyncEnumerableT = "System.Collections.Generic.IAsyncEnumerable`1";
+    public const string PipeReader = "System.IO.Pipelines.PipeReader";
+    public const string PipeWriter = "System.IO.Pipelines.PipeWriter";
+    public const string TextReader = "System.IO.TextReader";
+    public const string TextWriter = "System.IO.TextWriter";
+    public const string CancellationToken = "System.Threading.CancellationToken";
+    public const string ILogger = "Microsoft.Extensions.Logging.ILogger";
+    public const string ILoggerT = "Microsoft.Extensions.Logging.ILogger`1";
+}
+
+/// <summary>
+/// Provides utility methods for resolving types from a <see cref="Compilation"/>.
+/// </summary>
+public static class TypeResolutionExtensions
 {
     /// <summary>
     /// Resolves the best <see cref="INamedTypeSymbol"/> for the specified metadata name.
     /// </summary>
-    protected static INamedTypeSymbol? GetBestTypeByMetadataName(Compilation compilation, string metadataName)
+    public static INamedTypeSymbol? GetBestTypeByMetadataName(this Compilation compilation, string metadataName)
     {
         var type = compilation.GetTypeByMetadataName(metadataName);
         if (type != null) return type;
