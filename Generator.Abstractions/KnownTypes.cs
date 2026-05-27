8d521c919268262b376fd111bcb1b0aad1467637
juner <juner@juner.net>
2026-05-27 15:23:01 +0900

    feat: complete KnownTypes standardization and nullable handling in Generator.Abstractions

--- Generator.Abstractions/KnownTypes.cs
+++ Generator.Abstractions/KnownTypes.cs
@@ -3,23 +3,78 @@ using Microsoft.CodeAnalysis;
 namespace Esolang.Generator;
 
 /// <summary>
-/// Defines metadata names for commonly used types in esolang source generators.
+/// Holds resolved type symbols for a compilation.
 /// </summary>
-public static class KnownTypeNames
+public readonly struct KnownTypes
 {
-    public const string Task = "System.Threading.Tasks.Task";
-    public const string TaskT = "System.Threading.Tasks.Task`1";
-    public const string ValueTask = "System.Threading.Tasks.ValueTask";
-    public const string ValueTaskT = "System.Threading.Tasks.ValueTask`1";
-    public const string IEnumerableT = "System.Collections.Generic.IEnumerable`1";
-    public const string IAsyncEnumerableT = "System.Collections.Generic.IAsyncEnumerable`1";
-    public const string PipeReader = "System.IO.Pipelines.PipeReader";
-    public const string PipeWriter = "System.IO.Pipelines.PipeWriter";
-    public const string TextReader = "System.IO.TextReader";
-    public const string TextWriter = "System.IO.TextWriter";
-    public const string CancellationToken = "System.Threading.CancellationToken";
-    public const string ILogger = "Microsoft.Extensions.Logging.ILogger";
-    public const string ILoggerT = "Microsoft.Extensions.Logging.ILogger`1";
+#pragma warning disable CS1591
+    public readonly INamedTypeSymbol? String;
+    public readonly INamedTypeSymbol? Task;
+    public readonly INamedTypeSymbol? TaskT;
+    public readonly INamedTypeSymbol? ValueTask;
+    public readonly INamedTypeSymbol? ValueTaskT;
+    public readonly INamedTypeSymbol? IEnumerableT;
+    public readonly INamedTypeSymbol? IAsyncEnumerableT;
+    public readonly INamedTypeSymbol? PipeReader;
+    public readonly INamedTypeSymbol? PipeWriter;
+    public readonly INamedTypeSymbol? TextReader;
+    public readonly INamedTypeSymbol? TextWriter;
+    public readonly INamedTypeSymbol? CancellationToken;
+    public readonly INamedTypeSymbol? ILogger;
+    public readonly INamedTypeSymbol? ILoggerT;
+
+    public KnownTypes(Compilation compilation)
+    {
+        String = compilation.GetSpecialType(SpecialType.System_String);
+        
+        Task = compilation.GetBestTypeByMetadataName("System.Threading.Tasks.Task");
+        TaskT = compilation.GetBestTypeByMetadataName("System.Threading.Tasks.Task`1");
+        ValueTask = compilation.GetBestTypeByMetadataName("System.Threading.Tasks.ValueTask");
+        ValueTaskT = compilation.GetBestTypeByMetadataName("System.Threading.Tasks.ValueTask`1");
+        IEnumerableT = compilation.GetBestTypeByMetadataName("System.Collections.Generic.IEnumerable`1");
+        IAsyncEnumerableT = compilation.GetBestTypeByMetadataName("System.Collections.Generic.IAsyncEnumerable`1");
+        PipeReader = compilation.GetBestTypeByMetadataName("System.IO.Pipelines.PipeReader");
+        PipeWriter = compilation.GetBestTypeByMetadataName("System.IO.Pipelines.PipeWriter");
+        TextReader = compilation.GetBestTypeByMetadataName("System.IO.TextReader");
+        TextWriter = compilation.GetBestTypeByMetadataName("System.IO.TextWriter");
+        CancellationToken = compilation.GetBestTypeByMetadataName("System.Threading.CancellationToken");
+        ILogger = compilation.GetBestTypeByMetadataName("Microsoft.Extensions.Logging.ILogger");
+        ILoggerT = compilation.GetBestTypeByMetadataName("Microsoft.Extensions.Logging.ILogger`1");
+    }
+
+    private static ITypeSymbol? Unwrap(ITypeSymbol? type)
+    {
+        if (type is INamedTypeSymbol namedType && namedType.ConstructedFrom.SpecialType == SpecialType.System_Nullable_T)
+        {
+            return namedType.TypeArguments[0];
+        }
+        return type;
+    }
+
+    private static bool EqualsDefinition(ITypeSymbol? type, ISymbol? symbol) => 
+        type != null && symbol != null && SymbolEqualityComparer.Default.Equals(Unwrap(type)?.OriginalDefinition, symbol);
+    
+    private static bool EqualsType(ITypeSymbol? type, ISymbol? symbol) => 
+        type != null && symbol != null && SymbolEqualityComparer.Default.Equals(Unwrap(type)?.WithNullableAnnotation(NullableAnnotation.None), symbol);
+
+    public bool IsString(ITypeSymbol? type) => EqualsType(type, String);
+    public bool IsTask(ITypeSymbol? type) => EqualsType(type, Task);
+    public bool IsTaskT(ITypeSymbol? type) => EqualsDefinition(type, TaskT);
+    public bool IsValueTask(ITypeSymbol? type) => EqualsType(type, ValueTask);
+    public bool IsValueTaskT(ITypeSymbol? type) => EqualsDefinition(type, ValueTaskT);
+    public bool IsIEnumerableT(ITypeSymbol? type) => EqualsDefinition(type, IEnumerableT);
+    public bool IsIAsyncEnumerableT(ITypeSymbol? type) => EqualsDefinition(type, IAsyncEnumerableT);
+    public bool IsPipeReader(ITypeSymbol? type) => EqualsType(type, PipeReader);
+    public bool IsPipeWriter(ITypeSymbol? type) => EqualsType(type, PipeWriter);
+    public bool IsTextReader(ITypeSymbol? type) => EqualsType(type, TextReader);
+    public bool IsTextWriter(ITypeSymbol? type) => EqualsType(type, TextWriter);
+    public bool IsCancellationToken(ITypeSymbol? type) => EqualsType(type, CancellationToken);
+    public bool IsILogger(ITypeSymbol? type) => EqualsType(type, ILogger);
+    public bool IsILoggerT(ITypeSymbol? type) => EqualsDefinition(type, ILoggerT);
+    
+    public bool IsTaskString(ITypeSymbol? type) => IsTaskT(type) && ((INamedTypeSymbol)Unwrap(type)!).TypeArguments[0].SpecialType == SpecialType.System_String;
+    public bool IsValueTaskString(ITypeSymbol? type) => IsValueTaskT(type) && ((INamedTypeSymbol)Unwrap(type)!).TypeArguments[0].SpecialType == SpecialType.System_String;
+#pragma warning restore CS1591
 }
 
 /// <summary>
