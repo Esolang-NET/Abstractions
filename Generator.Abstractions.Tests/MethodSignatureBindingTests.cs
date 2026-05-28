0426647200ffdcb98c83b84243493b027732f49e
juner <juner@juner.net>
2026-05-28 17:37:29 +0900

    test(generator): MethodSignatureBindingTests の修正をコミット

--- Generator.Abstractions.Tests/MethodSignatureBindingTests.cs
+++ Generator.Abstractions.Tests/MethodSignatureBindingTests.cs
@@ -8,26 +8,32 @@ namespace Esolang.Generator.Tests;
 public class MethodSignatureBindingTests
 {
     [TestMethod]
-    public void PropertiesWorkAsExpected()
+    public void Properties_CheckCorrectly()
     {
-        var binding = new MethodSignatureBinding(
-            IsValid: true,
-            ReturnKind: MethodReturnKind.Void,
-            InputKind: MethodInputKind.None,
-            OutputKind: MethodOutputKind.None,
-            InputExpression: "",
-            OutputExpression: "",
-            CancellationTokenName: null,
-            LoggerExpression: null,
-            IsLoggerFromParameter: false,
-            UnhandledParameters: []
-        );
+        // IsAsync: Task 系
+        var bindingTask = new MethodSignatureBinding(true, MethodReturnKind.Task, MethodInputKind.None, MethodOutputKind.None, "", "", null, null, false, []);
+        Assert.IsTrue(bindingTask.IsAsync);
+        
+        // IsAsync: ValueTask 系
+        var bindingValueTask = new MethodSignatureBinding(true, MethodReturnKind.ValueTask, MethodInputKind.None, MethodOutputKind.None, "", "", null, null, false, []);
+        Assert.IsTrue(bindingValueTask.IsAsync);
 
-        Assert.IsTrue(binding.IsValid);
-        Assert.IsFalse(binding.HasExplicitInput);
-        Assert.IsFalse(binding.HasExplicitOutput);
-        Assert.IsFalse(binding.IsAsync);
-        Assert.IsFalse(binding.IsEnumerable);
-        Assert.IsFalse(binding.IsAsyncEnumerable);
+        // IsEnumerable
+        var bindingEnum = new MethodSignatureBinding(true, MethodReturnKind.IEnumerableByte, MethodInputKind.None, MethodOutputKind.None, "", "", null, null, false, []);
+        Assert.IsTrue(bindingEnum.IsEnumerable);
+        Assert.IsFalse(bindingEnum.IsAsync);
+
+        // IsAsyncEnumerable
+        var bindingAsyncEnum = new MethodSignatureBinding(true, MethodReturnKind.IAsyncEnumerableByte, MethodInputKind.None, MethodOutputKind.None, "", "", null, null, false, []);
+        Assert.IsTrue(bindingAsyncEnum.IsAsyncEnumerable);
+        Assert.IsTrue(bindingAsyncEnum.IsAsync);
+
+        // HasExplicitInput
+        var bindingInput = new MethodSignatureBinding(true, MethodReturnKind.Void, MethodInputKind.String, MethodOutputKind.None, "s", "", null, null, false, []);
+        Assert.IsTrue(bindingInput.HasExplicitInput);
+
+        // HasExplicitOutput
+        var bindingOutput = new MethodSignatureBinding(true, MethodReturnKind.Void, MethodInputKind.None, MethodOutputKind.TextWriter, "", "w", null, null, false, []);
+        Assert.IsTrue(bindingOutput.HasExplicitOutput);
     }
 }
